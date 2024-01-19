82223476fa9a3c82a8206fdab3f96049b1a513e2
wahfl2 <59855656+wahfl2@users.noreply.github.com>
2024-01-19 15:44:01 -0600

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <SDL.h>
+
+// You shouldn't really use this statement, but it's fine for small programs
+using namespace std;
+
+// You must include the command line parameters for your main function to be recognized by SDL
+int main(int argc, char** args) {
+
+    // Pointers to our window and surface
+    SDL_Surface* winSurface = NULL;
+    SDL_Window* window = NULL;
+
+    // Initialize SDL. SDL_Init will return -1 if it fails.
+    if ( SDL_Init( SDL_INIT_EVERYTHING ) < 0 ) {
+        cout << "Error initializing SDL: " << SDL_GetError() << endl;
+        system("pause");
+        // End the program
+        return 1;
+    }
+
+    // Create our window
+    window = SDL_CreateWindow(
+        "Example",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        680, 480,
+        SDL_WINDOW_SHOWN
+    );
+
+    // Make sure creating the window succeeded
+    if ( !window ) {
+        cout << "Error creating window: " << SDL_GetError()  << endl;
+        system("pause");
+        // End the program
+        return 1;
+    }
+
+    // Get the surface from the window
+    winSurface = SDL_GetWindowSurface( window );
+
+    // Make sure getting the surface succeeded
+    if ( !winSurface ) {
+        cout << "Error getting surface: " << SDL_GetError() << endl;
+        system("pause");
+        // End the program
+        return 1;
+    }
+
+    // Fill the window with a white rectangle
+    SDL_FillRect( winSurface, NULL, SDL_MapRGB( winSurface->format, 255, 255, 255));
+
+    // Update the window display
+    SDL_UpdateWindowSurface( window );
+
+    bool close = false;
+    while (!close) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    close = true;
+                break;
+            }
+        }
+
+        // calculates to 60 fps
+        SDL_Delay(1000 / 60);
+    }
+
+    SDL_DestroyWindow( window );
+    SDL_Quit();
+    return 0;
+}
